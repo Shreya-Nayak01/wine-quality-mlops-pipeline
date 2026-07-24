d944e235b01d2ad9a1413e8a6fc88b0068b88980
Shreya-Nayak01 <nayakshreya35@gmail.com>
2026-07-24 20:26:53 +0530

    data transformation added

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from wineQuality import logger
 from wineQuality.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from wineQuality.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from wineQuality.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+
 
 
 
@@ -21,6 +23,18 @@ STAGE_NAME = "Data Validation stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = DataValidationTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+
+STAGE_NAME = "Data Transformation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataTransformationTrainingPipeline()
    data_ingestion.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
